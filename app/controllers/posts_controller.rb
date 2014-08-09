6390f8f5de0a79ef2d3fe05b3403ab493a84bb40
Tyler Martinez <slightlytyler@gmail.com>
2014-08-09 15:07:40 -0500

    Postgres and minor changes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   require 'nokogiri'
 
   def index
-    feed = Feedjira::Feed.fetch_and_parse("http://iswiraq.blogspot.com/feeds/posts/default?start-index=1&max-results=100")
+    feed = Feedjira::Feed.fetch_and_parse("http://iswiraq.blogspot.com/feeds/posts/default?start-index=1&max-results=28")
     @feed_entries = feed.entries
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,2 @@
 class Post < ActiveRecord::Base
-  feed = Feedzirra::Feed.fetch_and_parse("http://iswiraq.blogspot.com/feeds/posts/default?alt=rss")
-  @entry = feed.entries
 end
