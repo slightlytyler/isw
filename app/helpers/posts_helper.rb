55b9210528866cab9f2421d64b391c937e0bcae1
Tyler Martinez <slightlytyler@gmail.com>
2014-08-11 16:06:27 -0500

    flickr integration

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -2,4 +2,8 @@ module PostsHelper
   def get_weeks_posts week, year
     return @posts = Post.order("published_at DESC").where(week: week, year: year)
   end
+
+  def get_home_image
+    return Post.home_background
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -18,35 +18,45 @@
 #
 
 class Post < ActiveRecord::Base
-  attr_accessible :guide, :name, :url, :published_at, :week, :month, :year, :image, :background_image, :content
-
-  def self.update_from_feed
-    isw_url = 'http://iswiraq.blogspot.com/feeds/posts/default?start-index=1&max-results=5000'
-    urls = [isw_url]
-    feeds = Feedjira::Feed.fetch_and_parse urls
-    isw_feed = feeds['http://iswiraq.blogspot.com/feeds/posts/default?start-index=1&max-results=5000']
+    attr_accessible :guide, :name, :url, :published_at, :week, :month, :year, :image, :background_image, :content
 
     FlickRaw.api_key="8e60dd79e0047a5c41f6f640cf5016e8"
     FlickRaw.shared_secret="8096d39dd9f30227"
     flickr_id = '50076792@N00'
-    flickr_feed = flickr.photos.search(:user_id => flickr_id)
+    @@flickr_feed = flickr.photos.search(:user_id => flickr_id, :sort => 'interestingness-desc')
 
-    isw_feed.entries.each do |entry|
-      if entry.title.include? 'Situation Report'
-        record = Post.find_or_initialize_by(guide: entry.id)
-        record.name          = entry.title
-        record.url           = entry.url
-        record.published_at  = entry.published
-        record.week          = entry.published.strftime('%W')
-        record.month         = entry.published.strftime('%M')
-        record.year          = entry.published.strftime('%Y')
-        record.guide         = entry.id
-        record.image         = Nokogiri::HTML(entry.content).css('img').map{ |i| i['src'] }[0]
-        record.content       = entry.content
-        record.save
-      end
+    def self.update_from_feed
+        isw_url = 'http://iswiraq.blogspot.com/feeds/posts/default?start-index=1&max-results=5000'
+        urls = [isw_url]
+        feeds = Feedjira::Feed.fetch_and_parse urls
+        isw_feed = feeds['http://iswiraq.blogspot.com/feeds/posts/default?start-index=1&max-results=5000']
+
+        isw_feed.entries.each_with_index do |entry, i|
+            if entry.title.include? 'Situation Report'
+                photo_id = @@flickr_feed[i].id
+                photo_info = flickr.photos.getInfo(:photo_id => photo_id)
+
+                record = Post.find_or_initialize_by(guide: entry.id)
+                record.name             = entry.title
+                record.url              = entry.url
+                record.published_at     = entry.published
+                record.week             = entry.published.strftime('%W')
+                record.month            = entry.published.strftime('%M')
+                record.year             = entry.published.strftime('%Y')
+                record.guide            = entry.id
+                record.image            = Nokogiri::HTML(entry.content).css('img').map{ |i| i['src'] }[0]
+                record.background_image = FlickRaw.url(photo_info)
+                record.content          = entry.content
+                record.save
+            end
+        end
     end
 
+    def self.home_background
 
-  end
+        home_photo_id = @@flickr_feed[0].id
+        home_photo_info = flickr.photos.getInfo(:photo_id => home_photo_id)
+        return FlickRaw.url(home_photo_info)
+    end
+
 end
